1fd6d2cd1866caa7f38a2260432de643a564d2a1
luandro <luandro@gmail.com>
2019-09-19 13:13:17 -0300

    check if url

--- electron/process/ui.js
+++ electron/process/ui.js
@@ -3,61 +3,79 @@ const WindowState = require('electron-window-state')
 const path = require('path')
 const join = require('../../lib/join')
 
-module.exports = function uiWindow (uiPath, opts = {}, config) {
-  uiPath = join('../..', uiPath)
+function validURL (str) {
+  var pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  ) // fragment locator
+  return !!pattern.test(str)
+}
 
+module.exports = function uiWindow (uiPath, opts = {}, config) {
   var windowState = WindowState({
     defaultWidth: 1024,
     defaultHeight: 768
   })
+  opts = Object.assign(
+    {
+      title: 'scuttlebutt ahoy!',
+      show: true,
 
-  opts = Object.assign({
-    title: 'scuttlebutt ahoy!',
-    show: true,
+      x: windowState.x,
+      y: windowState.y,
+      width: windowState.width,
+      height: windowState.height,
+      minWidth: 800,
 
-    x: windowState.x,
-    y: windowState.y,
-    width: windowState.width,
-    height: windowState.height,
-    minWidth: 800,
-
-    autoHideMenuBar: true,
-    frame: process.env.ELECTRON_FRAME !== 'false',
-    // titleBarStyle: 'hidden',
-    backgroundColor: '#fff',
-    icon: '../assets/icon.png' // TODO may need fixing
-  }, opts)
+      autoHideMenuBar: true,
+      frame: process.env.ELECTRON_FRAME !== 'false',
+      // titleBarStyle: 'hidden',
+      backgroundColor: '#fff',
+      icon: '../assets/icon.png' // TODO may need fixing
+    },
+    opts
+  )
 
   var win = new electron.BrowserWindow(opts)
   windowState.manage(win)
+  if (validURL(uiPath)) {
+    console.log('IS URL')
+    win.loadURL(uiPath)
+  } else {
+    uiPath = join('../..', uiPath)
+    win.webContents.on('dom-ready', function () {
+      win.webContents.executeJavaScript(Script(uiPath, opts, config))
+    })
 
-  win.webContents.on('dom-ready', function () {
-    win.webContents.executeJavaScript(Script(uiPath, opts, config))
-  })
+    function Script (uiPath, opts, config) {
+      return `
+        var electron = require('electron')
+        var h = require('mutant/h')
+        electron.webFrame.setVisualZoomLevelLimits(1, 1)
+        var title = ${JSON.stringify(opts.title || 'scuttlebutt ahoy!')}
+        document.documentElement.querySelector('head').appendChild(
+          h('title', title)
+        )
+        require('${uiPath}')(${JSON.stringify(config)})
+      `
+    }
 
-  function Script (uiPath, opts, config) {
-    return `
-      var electron = require('electron')
-      var h = require('mutant/h')
-      electron.webFrame.setVisualZoomLevelLimits(1, 1)
-      var title = ${JSON.stringify(opts.title || 'scuttlebutt ahoy!')}
-      document.documentElement.querySelector('head').appendChild(
-        h('title', title)
-      )
-      require('${uiPath}')(${JSON.stringify(config)})
-    `
+    win.webContents.on('will-navigate', function (e, url) {
+      e.preventDefault()
+      electron.shell.openExternal(url)
+    })
+
+    win.webContents.on('new-window', function (e, url) {
+      e.preventDefault()
+      electron.shell.openExternal(url)
+    })
+
+    win.loadURL('file://' + path.join(__dirname, '../assets/base.html'))
   }
-
-  win.webContents.on('will-navigate', function (e, url) {
-    e.preventDefault()
-    electron.shell.openExternal(url)
-  })
-
-  win.webContents.on('new-window', function (e, url) {
-    e.preventDefault()
-    electron.shell.openExternal(url)
-  })
-
-  win.loadURL('file://' + path.join(__dirname, '../assets/base.html'))
   return win
 }
